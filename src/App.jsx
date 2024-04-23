97682d77b10cb441a9fffd8d6f26129510513741
ThuongNVa <toanrui64@gmail.com>
2024-04-23 15:40:14 +0700

    Code Layout for Trello Clone Web

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import './App.css'
-import Button from '@mui/material/Button'
-import AccessAlarm from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
-import { useColorScheme } from '@mui/material/styles'
 // import useMediaQuery from '@mui/material/useMediaQuery'
-
+import { useColorScheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -16,6 +8,7 @@ import Select from '@mui/material/Select'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
 
 // CSR
 function ModeSelect() {
@@ -37,7 +30,7 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value='light'>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
             <LightModeIcon /> Light
           </Box>
         </MenuItem>
@@ -47,7 +40,7 @@ function ModeSelect() {
           </Box>
         </MenuItem>
         <MenuItem value='system'>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
             <SettingsBrightnessIcon /> System
           </Box>
         </MenuItem>
@@ -56,54 +49,33 @@ function ModeSelect() {
   )
 }
 
-function ModToggle() {
-  const { mode, setMode } = useColorScheme()
-
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
-  // console.log('Dark Mode: ', prefersDarkMode)
-  // console.log('Light Mode: ', prefersLightMode)
-
-  return (
-    <Button
-      onClick={ () => {
-        setMode( mode === 'light' ? 'dark' : 'light')
-        // localStorage.setItem('trello-web-dark-light-mode')
-        // localStorage.getItem('trello-web-dark-light-mode')
-      }}
-    >
-      {mode === 'light' ? 'Turn Dark' : 'Turn Light'}
-    </Button>
-  )
-}
-
 function App() {
-
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModToggle />
-      <hr />
-
-      <Typography variant='body2' color='text.secondary'>This is a test of Typography</Typography>
-      <Button variant="contained">Hello world</Button>
-
-      <br />
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-      <AccessAlarm />
-      <ThreeDRotation />
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>Board Bar</Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>Board Content</Box>
+    </Container>
   )
 }
 
