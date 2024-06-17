32fe3894219ea6fc718a6ea306de513aa5b306a4
ThuongNVa <toanrui64@gmail.com>
2024-06-17 21:01:21 +0700

    Code Board Content: Column (Footer and List Cards) #2

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -5,14 +5,24 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Divider from '@mui/material/Divider'
 import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import CardActions from '@mui/material/CardActions'
 import AddCardIcon from '@mui/icons-material/AddCard'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import MoveDownIcon from '@mui/icons-material/MoveDown'
 import CloudIcon from '@mui/icons-material/Cloud'
 import DeleteIcon from '@mui/icons-material/Delete'
 import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined'
+import AddIcon from '@mui/icons-material/Add'
+import PermMediaIcon from '@mui/icons-material/PermMedia'
+import CommentIcon from '@mui/icons-material/Comment'
+import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck'
+import AttachmentIcon from '@mui/icons-material/Attachment'
 import BackgroundImg from '~/assets/milin-john-sea-unsplash.jpg'
 
 const COLUMN_HEADER_HEIGHT = '50px'
@@ -48,6 +58,7 @@ function BoardContent() {
         margin: '10px',
         borderRadius: 3
       }}>
+        {/* Column Header (Column Title) */}
         <Box sx={{
           height: COLUMN_HEADER_HEIGHT,
           p: 3.5,
@@ -62,6 +73,7 @@ function BoardContent() {
           >
             Column Title
           </Typography>
+
           <Tooltip>
             <MoreHorizOutlinedIcon
               id="basic-column-dropdown"
@@ -80,6 +92,7 @@ function BoardContent() {
               }}
             />
           </Tooltip>
+
           <Menu
             id="basic-menu-column-dropdown"
             anchorEl={anchorEl}
@@ -122,9 +135,41 @@ function BoardContent() {
             </MenuItem>
           </Menu>
         </Box>
-        <Box sx={{}}>
-          List Card
+
+        {/* Column List Card (Column Cards) */}
+        <Box sx={{
+          p: '0 10px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1
+        }}>
+          <Card sx={{
+            boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+            cursor: 'pointer'
+          }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/448438955_122154611408129690_2479098467328202501_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=g2CQfuo1UckQ7kNvgG5KCT2&_nc_ht=scontent.fsgn2-6.fna&oh=00_AYCVFgvOtMwsF5TxqLRGwKXXfXm1ZAiErjoSJ1NM90MKKQ&oe=6675F549"
+              title="Test"
+            />
+            <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>Firefly / SAM</CardContent>
+            <CardActions sx={{
+              p: '0 4px 8px 4px'
+            }}>
+              <Button size='small' startIcon={<CommentIcon />}></Button>
+              <Button size='small' startIcon={<AttachmentIcon />}>2</Button>
+              <Button size='small' startIcon={<LibraryAddCheckIcon />}>0/4</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{
+            boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+            cursor: 'pointer'
+          }}>
+            <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>Firefly / SAM</CardContent>
+          </Card>
         </Box>
+
+        {/* Column Footer (Add a card) */}
         <Box sx={{
           height: COLUMN_FOOTER_HEIGHT,
           p: 3,
@@ -132,7 +177,31 @@ function BoardContent() {
           alignItems: 'center',
           justifyContent: 'space-between'
         }}>
-          Footer
+          <Button startIcon={<AddIcon />} sx={{
+            color: '#485460',
+            fontWeight: 'bold',
+            '&:hover': {
+              bgcolor: '#d2dae2',
+              color: '#485460',
+              borderRadius: 1.5
+            }
+          }}>
+            Add a card
+          </Button>
+
+          <Tooltip title='Create template card'>
+            <PermMediaIcon sx={{
+              minWidth: '40px',
+              cursor: 'pointer',
+              color: '#485460',
+              '&:hover': {
+                bgcolor: '#d2dae2',
+                color: '#485460',
+                borderRadius: 1.5
+              }
+            }}
+            />
+          </Tooltip>
         </Box>
       </Box>
     </Box>
