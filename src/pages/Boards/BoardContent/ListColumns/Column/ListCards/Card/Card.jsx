c1f0fb2e15151934711bb8a5ffc7af3517a960b5
ThuongNVa <toanrui64@gmail.com>
2024-08-03 18:29:36 +0700

    Drag And Drop Card #2

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -45,7 +45,8 @@ function TrelloCard({ card }) {
   const dndKitCardStyles = {
     transform: CSS.Translate.toString(transform),
     transition,
-    opacity: isDragging ? 0.5 : undefined
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? '2px solid #74b9ff' : undefined
   }
 
   return (
